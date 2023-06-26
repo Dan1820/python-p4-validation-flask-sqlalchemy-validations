45371af8747a382aa026c0aa26e876cf96cfdfff
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-06-26 12:38:06 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,6 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates, sessionmaker
 db = SQLAlchemy()
+
+connection_string = "sqlite:///database.db"   # for SQLite, local file
+db = create_engine(connection_string)
+base = declarative_base()
+
 
 class EmailAddress(db.Model):
     __tablename__ = 'emailaddress'
@@ -14,3 +23,18 @@ class EmailAddress(db.Model):
         if '@' not in address:
             raise ValueError("Failed simple email validation")
         return address
+
+
+Session = sessionmaker(db)
+session = Session()
+base.metadata.create_all(db)
+
+
+email = EmailAddress(email='banana')
+session.add(email)
+
+try:
+    session.commit()
+except SQLAlchemy.exc.IntegrityErr as e:
+    print("Integrity Violation blocked")
+    session.rollback()
